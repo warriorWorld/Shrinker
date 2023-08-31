33740983c138bab8b5ace819e4e02750f89fe93b
warriorWorld <772192594@qq.com>
2023-08-31 15:19:39 +0800

    dev...

--- app/src/main/java/com/harbinger/shrinker/MainActivity.kt
+++ app/src/main/java/com/harbinger/shrinker/MainActivity.kt
@@ -4,32 +4,28 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import com.harbinger.shrinker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    private fun initUI() {
+        findViewById<Button>(R.id.image_btn).setOnClickListener {
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        }
+        findViewById<Button>(R.id.gif_btn).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.video_btn).setOnClickListener {
+
         }
     }
 }
